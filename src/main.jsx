951801bbebda0482ab4bebfa155acc31b2958a5d
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-08 10:31:30 +0800

    resolve context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GlobalProvider } from './context/GlobalContext'
-import { AuthContext } from './context/auth/AuthContext'
+import { AuthContextProvider } from './context/auth/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <AuthContext>
+    <AuthContextProvider>
         <GlobalProvider>
             <App />
         </GlobalProvider>
-    </AuthContext>
+    </AuthContextProvider>
 )
