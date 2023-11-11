73106e8d9d3fe91e49ab55022bd4104df2a81bfa
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-11 14:56:19 +0800

    check token on local storage

--- src/context/auth/AuthContext.jsx
+++ src/context/auth/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { PropTypes } from 'prop-types'
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer, useEffect } from 'react'
 import AuthReducer from './AuthReducer'
 
 export const AuthContext = createContext()
@@ -8,6 +8,16 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AuthReducer, {
         user: null,
     })
+
+    useEffect(() => {
+        const token = localStorage.getItem('user')
+        if (token) {
+            dispatch({
+                type: 'LOGIN',
+                payload: { token },
+            })
+        }
+    }, [])
 
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
