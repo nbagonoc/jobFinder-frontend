1fe5cf03645e7faad8cfb8b15cdce025efcdb3fa
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-18 12:44:52 +0800

    preparing for rehaul on authState

--- src/context/auth/AuthContext.jsx
+++ src/context/auth/AuthContext.jsx
@@ -1,22 +1,28 @@
+// import { jwtDecode } from 'jwt-decode'
 import { PropTypes } from 'prop-types'
 import { createContext, useReducer, useEffect } from 'react'
-import AuthReducer from './AuthReducer'
 
+import AuthReducer from './AuthReducer'
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AuthReducer, {
         user: null,
+        email: null,
         alert: {},
         errors: [],
     })
 
     useEffect(() => {
         const token = localStorage.getItem('user')
+        // const decodedToken = jwtDecode(token)
+        // const email = decodedToken.role
+
         if (token) {
             dispatch({
                 type: 'LOGIN',
-                payload: { token },
+                payload: { token},
+                // payload: { token, email },
             })
         }
     }, [])
