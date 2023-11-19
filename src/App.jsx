a0224136a553fcc1884b18c8891108a9cf8043c3
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-19 11:03:26 +0800

    guard front-end routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,41 @@ import JobEdit from './pages/jobs/JobEdit'
 import Home from './pages/Home'
 import Navigation from './components/partials/Navigation'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+import { useAuthContext } from './hooks/useAuthContext'
 
 const App = () => {
+    const { user } = useAuthContext()
+
     return (
         <BrowserRouter>
-            <Navigation/>
-            <div className="container mt-3">
+            <Navigation />
+            <div className='container mt-3'>
                 <Routes>
-                    <Route path='/' element={<Home/>} />
+                    <Route path='/' element={<Home />} />
 
-                    <Route path='/login' element={<Login/>} />
-                    <Route path='/register' element={<Register/>} />
+                    {/* Auth */}
+                    <Route
+                        path='/login'
+                        element={user ? <Navigate to='/' /> : <Login />}
+                    />
+                    <Route
+                        path='/register'
+                        element={user ? <Navigate to='/' /> : <Register />}
+                    />
 
-                    <Route path='/jobs' element={<JobList/>} />
-                    <Route path='/jobs/view/:_id' element={<JobView/>} />
-                    <Route path='/jobs/create' element={<JobCreate/>} />
-                    <Route path='/jobs/edit/:_id' element={<JobEdit/>} />
+                    {/* Jobs */}
+                    <Route path='/jobs' element={<JobList />} />
+                    <Route path='/jobs/view/:_id' element={<JobView />} />
+                    <Route
+                        path='/jobs/create'
+                        element={ user && user.role === 'admin' ? <JobCreate />  : <Navigate to='/' />}
+                    />
+                    <Route
+                        path='/jobs/edit/:_id'
+                        element={ user && user.role === 'admin' ? <JobEdit />  : <Navigate to='/' />}
+                    />
                 </Routes>
             </div>
         </BrowserRouter>
