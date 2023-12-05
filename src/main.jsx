27e4fd135eff2c0e8c373fd006bad16c8b428fba
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-12-05 09:53:29 +0800

    Remove global context

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { GlobalProvider } from './context/GlobalContext'
 import { AuthContextProvider } from './context/auth/AuthContext'
 import { JobContextProvider } from './context/jobs/JobContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthContextProvider>
         <JobContextProvider>
-            <GlobalProvider>
-                <App />
-            </GlobalProvider>
+            <App />
         </JobContextProvider>
     </AuthContextProvider>
 )
