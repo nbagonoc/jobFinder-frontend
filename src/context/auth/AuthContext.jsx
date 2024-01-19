b8b81dadf1f96df39a0ef76e2e6ebf97276ab01d
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-01-19 21:18:30 +0800

    refactoring

--- src/context/auth/AuthContext.jsx
+++ src/context/auth/AuthContext.jsx
@@ -18,17 +18,22 @@ export const AuthContextProvider = ({ children }) => {
 
         if (token) {
             const decodedToken = jwtDecode(token)
-            const user = { _id: decodedToken._id, name: decodedToken.firstName, role: decodedToken.role }
+            const user = {
+                _id: decodedToken._id,
+                name: decodedToken.firstName,
+                role: decodedToken.role,
+            }
             const currentTime = Date.now() / 1000
 
-            if (decodedToken.exp && decodedToken.exp < currentTime) { // Check if the token is expired
+            if (decodedToken.exp && decodedToken.exp < currentTime) {
+                // Check if the token is expired
                 localStorage.removeItem('token')
                 dispatch({ type: 'LOGOUT' })
             } else {
                 dispatch({
                     type: 'LOGIN',
                     payload: { user, token },
-                });
+                })
             }
         }
     }, [])
